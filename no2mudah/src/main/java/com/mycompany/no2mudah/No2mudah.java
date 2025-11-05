4f2ab74af8f68445597b38652a569817de4a5726
nagatabumi <nagataanggerbumi@gmail.com>
2025-11-05 07:06:16 +0700

    Add files via upload

---
+++ no2mudah/src/main/java/com/mycompany/no2mudah/No2mudah.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.no2mudah;
+
+/**
+ *
+ * @author LENOVO
+ */
+import java.util.Scanner;
+
+public class No2mudah {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah bilangan: ");
+        int bilangan = input.nextInt();
+
+        if (bilangan % 2 == 0) {
+            System.out.println("Bilangan " + bilangan + " adalah GENAP.");
+        } else {
+            System.out.println("Bilangan " + bilangan + " adalah GANJIL.");
+        }
+    }
+}
